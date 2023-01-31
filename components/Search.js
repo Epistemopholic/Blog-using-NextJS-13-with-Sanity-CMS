942d658624154c7f6d8a889d0ecf2804edd2b236
Epistemopholic <CODR.exe@gmail.com>
2023-01-31 19:33:46 +0500

    update

--- components/Search.js
+++ components/Search.js
@@ -1,6 +1,11 @@
 import React from "react";
-
+//import { useState } from "react";
 function Search() {
+  /*const [query, setQuery] = useState[""];
+  const getQuery = (event) => {
+    setQuery(event.target.value);
+  };
+  */
   return (
     <form>
       <label
@@ -30,6 +35,7 @@ function Search() {
         <input
           type="search"
           id="default-search"
+          //value={query}
           className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-violet-500 focus:border-violet-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"
           placeholder="Type something here.."
           required
@@ -37,6 +43,7 @@ function Search() {
         <button
           type="submit"
           className="text-white absolute right-2.5 bottom-2.5 bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800"
+          //onClick={getQuery}
         >
           Search
         </button>
